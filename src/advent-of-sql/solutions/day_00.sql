aadcf972cc18fb90db6ba09d5d88326e7cafc371
Bill <bill@thewallisfamily.co.uk>
2024-11-08 19:42:36 +0000

    ✨ feat: add Advent of SQL example problem

---
+++ src/advent-of-sql/solutions/day_00.sql
@@ -0,0 +1,56 @@
+
+use santa_workshop;
+
+/* Children */
+select * from Children;
+
+/* ChristmasList */
+select * from ChristmasList;
+
+/* Gifts */
+select * from Gifts;
+
+/* Reindeer */
+select * from Reindeer;
+
+
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+
+/* Solution */
+with
+
+children_with_delivered_gifts as (
+    select *
+    from Children
+    where exists(
+        select *
+        from ChristmasList
+            inner join Gifts
+                using (gift_id)
+        where 1=1
+            and ChristmasList.child_id = Children.child_id
+            and ChristmasList.was_delivered
+    )
+    qualify count(*) over (partition by country, city) >= 5
+),
+
+city_scores as (
+    select
+        country,
+        city,
+        avg(naughty_nice_score) as avg_score
+    from children_with_delivered_gifts
+    group by all
+)
+
+select
+    *,
+    rank() over (partition by country, city order by avg_score desc) as city_rank
+from city_scores
+qualify city_rank <= 3
+order by
+    avg_score desc,
+    country,
+    city
+;
